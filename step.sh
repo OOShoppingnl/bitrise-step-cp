c4407b5829abcc1606bf00e103f7f393785f9dd8
Sebastiaan Eekhof <sebastiaan.eekhof@hatsa.com>
2019-10-30 15:12:35 +0100

    Added automatic folder creation

--- step.sh
+++ step.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 set -e
 set -x
-cp -R $cp_from $cp_to
+
+File_Name=$(basename "$cp_to")
+Dir_Path="${cp_to/$File_Name}"
+Dir_Path=${Dir_Path%?}
+
+echo $Dir_Path
+
+if [[ -d "$Dir_Path" ]]; then
+  echo "$Dir_Path exists"
+  else
+  mkdir -p $Dir_Path
+  echo "Created directory: $Dir_Path"
+fi
+
+cp $cp_from $cp_to
+
 envman add --key NEW_LOCATION -- value $cp_to
